c1c939e74d77bb7d3286e800b96c646ad3123788
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-08-16 21:30:58 +0300

    Add Combatant, Islander, Soldier, Town, Feature, and Loot classes; update Pirate and PirateGame for enhanced gameplay mechanics

---
+++ Udemy Cource/src/MasteringMutability/Challenge/Islander.java
@@ -0,0 +1,9 @@
+package MasteringMutability.Challenge;
+
+public final class Islander extends Combatant {
+
+    public Islander(String name, Weapon weapon) {
+        super(name);
+        setCurrentWeapon(weapon);
+    }
+}
---
+++ Udemy Cource/src/MasteringMutability/Challenge/Soldier.java
@@ -0,0 +1,9 @@
+package MasteringMutability.Challenge;
+
+public final class Soldier extends Combatant {
+
+    public Soldier(String name, Weapon weapon) {
+        super(name);
+        setCurrentWeapon(weapon);
+    }
+}
