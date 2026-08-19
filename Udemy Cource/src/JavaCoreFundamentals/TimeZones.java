23b5707efca6f787b2fe092bb9e6f7e3de1ccefa
AlexDDimitrov <aleks.dim.dimitrov@gmail.com>
2026-08-19 17:34:07 +0300

    Add various Java classes for date, time, localization, and randomization; implement game mechanics and user interaction

---
+++ Udemy Cource/src/JavaCoreFundamentals/TimeZones.java
@@ -0,0 +1,38 @@
+package JavaCoreFundamentals;
+
+import java.time.*;
+import java.time.ZoneId;
+import java.util.*;
+
+public class TimeZones {
+    static void main(String[] args) {
+        System.out.println(ZoneId.systemDefault());
+
+        System.out.println(ZoneId.getAvailableZoneIds().size());
+
+        System.setProperty("user.timezone", "America/Los_Angeles");
+        System.out.println(ZoneId.systemDefault());
+
+        System.out.println("Number of TZs = " + ZoneId.getAvailableZoneIds().size());
+        ZoneId.getAvailableZoneIds().stream()
+                .filter(s -> s.startsWith("US"))
+                .sorted()
+                .map(ZoneId::of)
+                .forEach(z -> System.out.println(z.getId() + ": " + z.getRules()));
+
+        Set<String> jdk8Zones = ZoneId.getAvailableZoneIds();
+        String[] alternate = TimeZone.getAvailableIDs();
+        Set<String> oldway = new HashSet<>(Set.of(alternate));
+
+        oldway.removeAll(jdk8Zones);
+        System.out.println(oldway);
+        ZoneId bet = ZoneId.of("BET", ZoneId.SHORT_IDS);
+        System.out.println(bet);
+
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+
+        Instant instantNow = Instant.now();
+        System.out.println(instantNow);
+    }
+}
